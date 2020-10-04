1bb9758522ead9bac6bdf0a1e4032107b6627379
Hoan Le <lenguyenhanhoan@gmail.com>
2020-10-04 09:43:08 +0700

    paging

---
+++ Models/Paging/PagingRequest.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoTutorialDemo.Models.Paging
+{
+    public class PagingRequest
+    {
+        [DefaultValue(1)]
+        [JsonProperty(PropertyName = "currentPage")]
+        public int CurrentPage { get; set; }
+
+        [DefaultValue(10)]
+        [JsonProperty(PropertyName = "itemsPerPage")]
+        public int ItemsPerPage { get; set; }
+    }
+}
---
+++ Models/Paging/PagingResult.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MongoTutorialDemo.Models.Paging
+{
+    public class PagingResult
+    {
+        [DefaultValue(0)]
+        public int MaxItemCount { get; set; }
+
+        [DefaultValue(1)]
+        public int CurrentPage { get; set; }
+
+        [DefaultValue(10)]
+        public int ItemsPerPage { get; set; }
+
+        public IEnumerable Items { get; set; }
+
+        [DefaultValue(true)]
+        public bool HasNextPage { get { return MaxItemCount >= CurrentPage * ItemsPerPage; } }
+
+        [DefaultValue(true)]
+        public bool HasPreviousPage { get { return CurrentPage > 1; } }
+    }
+}
